f85cbb9bccdc559ed5d5cdfb607c646f4204bdb5
RazmikYepremyan <razmikyepremyan@gmail.com>
2025-07-30 00:38:27 +0400

    picture_examples

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 model = YOLO('best.pt')  
-results=model("images/3.jpeg")
-results[0].show()
+results=model("../images/64.jpeg")
+results[0].save()
 # if you want to get coordinates
 boxes = results[0].boxes
 xyxy_np = boxes.xyxy.cpu().numpy()
